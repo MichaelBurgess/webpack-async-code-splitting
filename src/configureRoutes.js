e9a1d882656d9d2dabbdcd77d7d23207fba54b11
Michael Burgess <michael@mnetuk.com>
2017-05-18 13:51:00 +0100

    Rename references to asyncRoute

--- src/configureRoutes.js
+++ src/configureRoutes.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom/es";
-import asyncComponent from "./asyncComponent";
+import asyncRoute from "./asyncRoute";
 
 const configureRoutes = reducerRegistry => {
   return (
     <Switch>
-      <Route path="/home" component={asyncComponent(
+      <Route path="/home" component={asyncRoute(
         () => import(/* webpackChunkName: "home" */ "./features/Home/reducers"),
         () => import(/* webpackChunkName: "home" */ "./features/Home"),
         reducerRegistry
